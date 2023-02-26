6228d2fed5a3d680c1e7e229f83ef2a05e133c35
Webdasturchi2223 <webdasturchi2223@gmail.com>
2023-02-26 12:00:44 +0300

    commit

---
+++ js/app.js
@@ -0,0 +1,24 @@
+jQuery(document).ready(function($) {
+  var icon = $(".menu");
+  var menu = $(".mobile");
+  var tl = new TimelineLite({ paused: true, reversed: true });
+  tl.fromTo(
+    ".mobile",
+    0.5,
+    {
+      x: -200,
+      autoAlpha: 0
+    },
+    {
+      x: 0,
+      autoAlpha: 1,
+      ease: Power4.easeOut
+    }
+  );
+  icon.click(function() {
+    tl.play();
+  });
+  $(".close__menu").click(function() {
+    tl.reverse();
+  });
+});
