7ef8917b7fdbe4c3d01d8807d2860e5201e52ec3
Webdasturchi2223 <webdasturchi2223@gmail.com>
2023-02-27 21:24:22 +0300

    commit

--- js/scroll.js
+++ js/scroll.js
@@ -1,5 +1,22 @@
+$(document).ready(function () {
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 100) {
+			$('.scroll-top').fadeIn();
+		} else {
+			$('.scroll-top').fadeOut();
+		}
+	});
+	// scroll body to 0px on click
+	$('.scroll-top').click(function () {
+		$('body,html').animate({
+			scrollTop: 0
+		}, 400);
+		return false;
+	});
+});
+
 let scrollTop = document.querySelector('.scroll-top')
 
-window.addEventListener('scroll' , function(e){
-   scrollTop.style.transform = `rotate(${+window.pageYOffset + "deg"})`
+window.addEventListener('scroll', function (e) {
+	scrollTop.style.transform = `rotate(${+window.pageYOffset + "deg"})`
 })
